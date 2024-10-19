e65ba9c16c79450384f00f90d455bf5c820b0388
Bade Lalit <122799145+lalitbade@users.noreply.github.com>
2024-10-19 22:09:27 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -3,22 +3,12 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import summarizeRoutes from './routes/summarize.js'; 
-
-// Load environment variables from .env file
 dotenv.config();
-
-// Create an Express application
 const app = express();
 const PORT = process.env.PORT || 5001;
-
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
-// API Routes
 app.use('/api', summarizeRoutes); 
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
